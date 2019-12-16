d6bd86ca1cc84311a6a47b969b6541f69c135cff
Ati Ok <ao10@umbc.edu>
2019-12-16 02:50:11 -0500

    Add Jest and directory of skeleton tests

---
+++ app/test/routes/index.test.js
@@ -0,0 +1,7 @@
+'use strict';
+
+describe('index routes test', () => {
+  it('should test the logic of all the routes in index.js', () => {
+
+  });
+});
---
+++ app/test/routes/users.test.js
@@ -0,0 +1,7 @@
+'use strict';
+
+describe('users routes test', () => {
+  it('should test the logic of all the routes in users.js', () => {
+    
+  });
+});
