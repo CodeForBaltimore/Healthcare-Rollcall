a6bdfd73565d18f7808fa35867240f2e389a561b
Ati Ok <ao10@umbc.edu>
2019-12-16 03:02:19 -0500

    Remove Jade from generator/ and render JSON in index route

--- app/app.js
+++ app/app.js
@@ -9,10 +9,6 @@ var usersRouter = require('./routes/users');
 
 var app = express();
 
-// view engine setup
-app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'jade');
-
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
--- app/generator/app.js
+++ app/generator/app.js
@@ -11,7 +11,6 @@ var app = express();
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'jade');
 
 app.use(logger('dev'));
 app.use(express.json());
