409ab654255198ce2c521e49078f279149af59b3
Jason Anton <rev.jt.anton@gmail.com>
2020-03-19 16:41:48 -0400

    trying dotenv...it did not work

--- src/utils/api.js
+++ src/utils/api.js
@@ -1,6 +1,7 @@
 import axios from 'axios';
 
 export const postLogin = (email, password) => {
+    console.log(process.env.BASE_API_URL)
     const fullUrl = new URL('/user/login', process.env.BASE_API_URL);
     return axios.post(
         fullUrl.toString(),
