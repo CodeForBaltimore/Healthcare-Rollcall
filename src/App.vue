1c81f667f0a2dd83c19fac158474538a2a531cc3
Michael Gardner <mgardner@bellese.io>
2020-03-31 10:46:50 -0400

    disable back button for select pages

--- src/App.vue
+++ src/App.vue
@@ -13,7 +13,7 @@
         <b-navbar-toggle target="nav-collapse"></b-navbar-toggle>
         <b-collapse id="nav-collapse" is-nav>
           <b-navbar-nav>
-            <b-nav-item v-on:click="goBack()" v-if="this.$root.auth_state && this.$router.currentRoute.name !== 'dashboard'">&larr; Go Back</b-nav-item>
+            <b-nav-item v-on:click="goBack()" v-if="this.$root.auth_state && !disableBackBtn.includes(this.$router.currentRoute.name)">&larr; Go Back</b-nav-item>
           </b-navbar-nav>
           <b-navbar-nav class="ml-auto" v-if="this.$root.auth_state">
             <b-nav-item-dropdown right>
@@ -41,7 +41,8 @@
     name: "App",
     data() {
       return {
-        user: ""
+        user: "",
+        disableBackBtn: ['dashboard', 'create-contact', 'update-contact']
       }
     },
     created() {
