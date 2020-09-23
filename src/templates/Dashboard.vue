c8d9e2c0fb641f96a9d0db7f1aa6c11cbe1cb03b
ijadams <ijadams503@gmail.com>
2020-09-23 14:34:43 -0500

    add email modal

--- src/templates/Dashboard.vue
+++ src/templates/Dashboard.vue
@@ -5,25 +5,27 @@
         <h1>Provider Status Dashboard</h1>
         <h4 class="text-muted">Status &amp; Check-in Starting Point</h4>
         <p
-          class="lead"
-        >Choose a Provider from the list below to begin Check-in. You can edit all provider info once inside their page.</p>
+            class="lead"
+        >Choose a Provider from the list below to begin Check-in. You can edit all provider info once inside their
+          page.</p>
         <!-- Dashboard Table -->
         <b-row>
           <b-col cols="4">
             <b-form-group label="Filter" label-align="left">
               <b-input-group>
                 <b-form-input
-                  v-model="filter.keyword"
-                  type="search"
-                  id="dashboard-table"
-                  placeholder="Type to Search"
+                    v-model="filter.keyword"
+                    type="search"
+                    id="dashboard-table"
+                    placeholder="Type to Search"
                 ></b-form-input>
                 <b-input-group-append>
                   <b-button
-                    :disabled="!filter.keyword"
-                    @click="filter.keyword = ''"
-                    variant="outline-primary"
-                  >Clear</b-button>
+                      :disabled="!filter.keyword"
+                      @click="filter.keyword = ''"
+                      variant="outline-primary"
+                  >Clear
+                  </b-button>
                 </b-input-group-append>
               </b-input-group>
             </b-form-group>
@@ -35,24 +37,35 @@
               </b-input-group>
             </b-form-group>
           </b-col>
-          <div v-if="showAdmin" cols="4" class="center">
-            <b-button v-on:click="addFacility()" id="create-facility--btn">Create Facility</b-button>
+          <b-col cols="4" class="center" id="facility--btn--container">
+            <b-button v-if="showAdmin" v-on:click="addFacility()">Create Facility</b-button>
+            <b-button v-if="showAdmin || showUser" v-b-modal.email-modal>Email Facilities</b-button>
+          </b-col>
+          <div>
+            <b-modal id="email-modal" title="Send Emails to Facilities" ok-title="Send" @ok="sendEmails">
+              <b-form-group label="Select a facility type to send bulk emails" label-align="left">
+                <b-form-radio required v-for="(type, i) in facilityTypes" :key="i" v-model="facilityTypeSelected"
+                              v-bind:value="type">{{ type }}
+                </b-form-radio>
+                <p class="error" v-if="showEmailErr">Type is required</p>
+              </b-form-group>
+            </b-modal>
           </div>
         </b-row>
         <b-table
-          id="dashboard-table"
-          striped
-          hover
-          :per-page="perPage"
-          :current-page="currentPage"
-          :sort-by.sync="sortBy"
-          :sort-desc.sync="sortDesc"
-          :sort-direction="sortDirection"
-          @filtered="onFiltered"
-          :items="entities"
-          :filter="filter"
-          :filter-function="filterRow"
-          :fields="[
+            id="dashboard-table"
+            striped
+            hover
+            :per-page="perPage"
+            :current-page="currentPage"
+            :sort-by.sync="sortBy"
+            :sort-desc.sync="sortDesc"
+            :sort-direction="sortDirection"
+            @filtered="onFiltered"
+            :items="entities"
+            :filter="filter"
+            :filter-function="filterRow"
+            :fields="[
             {
               key: 'name', stickyColumn: true, isRowHeader: true,
               sortable: true
@@ -72,15 +85,14 @@
               formatter: value => {
                 return this.$options.filters.timestamp(value);
               }
-              // Variant applies to the whole column, including the header and footer
-              //variant: 'danger'
             }
           ]"
         >
           <template v-slot:cell(name)="data">
             <router-link
-              :to="{ name: 'facility', params: { entityID: data.item.id }}"
-            >{{ data.item.name }}</router-link>
+                :to="{ name: 'facility', params: { entityID: data.item.id }}"
+            >{{ data.item.name }}
+            </router-link>
           </template>
           <template v-slot:cell(type)="data">{{ data.item.type }}</template>
           <template v-slot:cell(status)="data">
@@ -94,11 +106,16 @@
 </template>
 
 <script>
+import {FACILITY_TYPES} from '../constants/facilities.const';
+
 export default {
   name: "Dashboard",
   data() {
     const showAdmin =
-      this.$jwt.decode(this.$root.auth_token).type === "admin" ? true : false
+        this.$jwt.decode(this.$root.auth_token).type === "admin" ? true : false
+    const showUser =
+        this.$jwt.decode(this.$root.auth_token).type === "user" ? true : false
+
     return {
       rows: 0,
       perPage: 25,
@@ -110,14 +127,18 @@ export default {
       filterOn: ["name"],
       statusOptions: [],
       showAdmin,
+      showUser,
+      facilityTypes: FACILITY_TYPES,
+      facilityTypeSelected: null,
+      showEmailErr: false,
       entities: null,
       sortBy: "updated",
       sortDesc: false,
       sortDirection: "asc",
       fields: [
-        { key: "name", sortable: true },
-        { key: "status", sortable: true },
-        { key: "updatedAt", sortable: true }
+        {key: "name", sortable: true},
+        {key: "status", sortable: true},
+        {key: "updatedAt", sortable: true}
       ]
     }
   },
@@ -127,16 +148,43 @@ export default {
       this.rows = this.entities.length
       for (let entity of this.entities) {
         if (
-          entity.checkIn &&
-          entity.checkIn.checkIns &&
-          entity.checkIn.checkIns.length > 0
+            entity.checkIn &&
+            entity.checkIn.checkIns &&
+            entity.checkIn.checkIns.length > 0
         ) {
           entity.status =
-            entity.checkIn.checkIns[entity.checkIn.checkIns.length - 1].status
+              entity.checkIn.checkIns[entity.checkIn.checkIns.length - 1].status
         } else {
           entity.status = "No Previous Check-in"
         }
       }
+    },
+    sendEmails(bvModalEvt) {
+      bvModalEvt.preventDefault()
+      if (!this.facilityTypeSelected) {
+        this.showEmailErr = true;
+        return
+      }
+      // send emails
+      const payload = {
+        "relationshipTitle": [this.facilityTypeSelected]
+      };
+      this.$root.apiPOSTRequest("/contact/send", payload, this.handleSendResponse)
+      // close modal
+      this.$nextTick(() => {
+        this.showEmailErr = false;
+        this.$bvModal.hide('email-modal')
+      })
+    },
+    handleSendResponse(response) {
+      const title = response.results ? 'Success' : 'Error';
+      const variant = response.results ? 'success' : 'danger';
+      const msg = response.results ? `Success: Emails sent to ${response.results.total}` : 'Error: Failed to send emails';
+      this.$bvToast.toast(msg, {
+        title,
+        variant,
+        solid: true
+      })
     },
     onFiltered(filteredItems) {
       this.rows = filteredItems.length
@@ -145,13 +193,13 @@ export default {
     filterRow(row, filter) {
       let match = !filter.status || row.status === this.filter.status
       return match && filter.keyword
-        ? this.filterOn.reduce(
-            (match, field) =>
-              match ||
-              row[field].toLowerCase().match(filter.keyword.toLowerCase()),
-            false
+          ? this.filterOn.reduce(
+              (match, field) =>
+                  match ||
+                  row[field].toLowerCase().match(filter.keyword.toLowerCase()),
+              false
           )
-        : match
+          : match
     },
     addFacility() {
       this.$router.push({
@@ -161,14 +209,13 @@ export default {
   },
   mounted() {
     let options = this.$root.getStatuses().map(status => {
-      return { value: status, text: status, disabled: false }
+      return {value: status, text: status, disabled: false}
     })
-    this.statusOptions = [{ value: null, text: "All" }, ...options]
+    this.statusOptions = [{value: null, text: "All"}, ...options]
     this.$root.apiGETRequest("/entity", this.updateEntities)
   }
 }
 </script>
-
 
 
 <style lang="scss">
@@ -176,18 +223,22 @@ h1,
 h3 {
   text-align: left;
 }
+
 table tr > th,
 table tr > td {
   text-align: left !important;
 }
+
 ul.pagination {
   display: block;
   margin: auto;
   text-align: center;
 }
+
 ul.pagination > li {
   display: inline-block;
 }
+
 p.lead {
   margin: 30px 0 15px;
 }
@@ -198,8 +249,19 @@ p.lead {
   align-items: center;
 }
 
-#create-facility--btn {
-  margin-top: 1rem;
+#facility--btn--container button {
+  margin: 1rem 0.25rem 0 0.25rem;
+  line-height: 100%;
+  width: 100%;
+}
+
+#email-modal {
+  p.error {
+    width: 100%;
+    margin-top: .25rem;
+    font-size: 80%;
+    color: #dc3545;
+  }
 }
 
 </style>
