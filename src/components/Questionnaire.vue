8aa08023b6a6a7821e3832e27dc743bbedab9434
lbarbula-bellese <lbarbula@bellese.io>
2020-10-29 13:49:36 -0600

    adding boiler plate for questionnaire

---
+++ src/components/Questionnaire.vue
@@ -0,0 +1,103 @@
+<template>
+  <div>
+    <b-form @submit="submitQuestionnaire" v-if="questionnaire.questions">
+      <b-form-group
+        v-for="(question, index) in questionnaire.questions"
+        :key="index"
+      >
+        <label> {{ question.questionText }}</label>
+        <b-form-radio-group
+          :required="question.required ? true : false"
+          v-if="question.responseType === 'single-select'"
+        >
+          <div
+            v-for="(response, index) in question.validResponses"
+            :key="index"
+          >
+            <b-radio value="response">{{ response }}</b-radio>
+          </div>
+        </b-form-radio-group>
+        <b-form-checkbox-group
+          :required="question.required ? true : false"
+          v-if="question.responseType === 'multi-select'"
+        >
+          <div
+            v-for="(response, index) in question.validResponses"
+            :key="index"
+          >
+            <b-checkbox value="response">{{ response }}</b-checkbox>
+          </div>
+        </b-form-checkbox-group>
+        <b-form-input
+          :required="question.required ? true : false"
+          v-if="question.responseType === 'short-text'"
+        ></b-form-input>
+        <b-form-textarea
+          :required="question.required ? true : false"
+          v-if="question.responseType === 'long-text'"
+        ></b-form-textarea>
+      </b-form-group>
+    </b-form>
+  </div>
+</template>
+<script>
+export default {
+  name: "questionnaire",
+  props: [],
+  data() {
+    return {
+      questionnaire: {
+        questions: [
+          {
+            index: 0,
+            required: true,
+            questionText: "Test Text",
+            responseType: "short-text",
+            validResponses: ["Poor", "Not Great", "Fantastic"],
+            allowOther: false,
+          },
+          {
+            index: 0,
+            required: true,
+            questionText: "Test Text",
+            responseType: "long-text",
+            validResponses: ["Poor", "Not Great", "Fantastic"],
+            allowOther: false,
+          },
+          {
+            index: 0,
+            required: true,
+            questionText: "Test Text",
+            responseType: "single-select",
+            validResponses: ["Poor", "Not Great", "Fantastic"],
+            allowOther: false,
+          },
+          {
+            index: 0,
+            required: true,
+            questionText: "Test Text",
+            responseType: "multi-select",
+            validResponses: ["Poor", "Not Great", "Fantastic"],
+            allowOther: false,
+          },
+        ],
+      },
+    };
+  },
+  methods: {
+    submitQuestionnaire(questionnaire) {
+      console.log(questionnaire);
+    },
+    createQuestionnaireObject(questionnaire) {
+      if (questionnaire.questions) {
+        //parse questions and create structure
+        // this.questions
+        this.questionnaire = questionnaire;
+      }
+    },
+    getQuestionnaire() {
+      "/questionnaires", this.createQuestionnaireObject();
+    },
+  },
+};
+</script>
