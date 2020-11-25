024d740ae7aacf3933983e94087daad8a1e5834a
Josh Glazer <joshglazer@gmail.com>
2020-11-25 16:39:12 -0500

    fix bugs related to the button that shows that allows a user to email a specific facility add an edit button to the facility's action table cell

--- src/templates/Dashboard.vue
+++ src/templates/Dashboard.vue
@@ -40,20 +40,25 @@
           <b-col cols="5" class="align--right mt-3 btn--container">
             <b-button v-if="showAdmin" v-on:click="addFacility()">Create Facility</b-button>
             <b-button v-if="showAdmin" class="ml-1" v-on:click="downloadCSV()">Download CSV</b-button>
-            <b-button v-if="showAdmin || showUser"  class="ml-1" v-b-modal.bulk-email-modal>
+            <b-button v-if="showAdmin || showUser" @click="actionedFacility = null" class="ml-1" v-b-modal.email-facility-modal>
               Email 
               {{ selectedEntityIds.length ? 'Selected' : '' }}
               Facilities
             </b-button>
           </b-col>
           <div>
-            <b-modal id="bulk-email-modal" title="Send Emails to Facilities" ok-title="Send" @ok="sendEmails">
-              <div v-if="selectedEntityIds.length">
+            <b-modal id="email-facility-modal" title="Send Emails to Facilities" ok-title="Send" @ok="sendEmails">              
+              <div v-if="selectedEntityIds.length || actionedFacility">
                 Are you sure you want to send an email to the following facilities?
                 <ul>
-                  <li v-for="(entityName, i) in entities.filter(entity => selectedEntityIds.includes(entity.id)).map(entity => entity.name).sort()" :key="i">
-                    <div>{{ entityName }}</div>
-                  </li>
+                  <template v-if="actionedFacility">
+                    <li>{{ actionedFacility.name }}</li>
+                  </template>
+                  <template v-else>
+                    <li v-for="(entityName, i) in entities.filter(entity => selectedEntityIds.includes(entity.id)).map(entity => entity.name).sort()" :key="i">
+                      <div>{{ entityName }}</div>
+                    </li>
+                  </template>
                 </ul>
               </div>
               <div v-else>
@@ -64,9 +69,6 @@
                   <p class="error" v-if="showEmailErr">Type is required</p>
                 </b-form-group>
               </div>
-            </b-modal>
-            <b-modal id="single-email-modal" title="Send Email to Facility" ok-title="Send" @ok="sendEmail">
-              <p>Are you sure you want to send an email to {{ actionedFacility ? actionedFacility.name : 'this facility' }}?</p>
             </b-modal>
           </div>
         </b-row>
@@ -151,9 +153,20 @@
             <span>{{ data.item.status }}</span>
           </template>
           <template v-slot:cell(actions)="data">
-            <span v-if="data.item.email && showAdmin" class="clickable" @click="actionedFacility = data.item" v-b-modal.single-email-modal>
-              <b-icon-envelope-fill></b-icon-envelope-fill>
-            </span>
+            <div class="tableIcons">
+              <a v-if="showAdmin" class="clickable">
+                <router-link
+                  :to="{ name: 'facility-edit', params: { entityID: data.item.id }}"
+                >
+                  <b-icon-pencil></b-icon-pencil>
+                  <span class="sr-only">Edit {{ data.item.name }}</span>
+                </router-link>
+              </a>
+              <a v-if="showAdmin" class="clickable" @click="actionedFacility = data.item" v-b-modal.email-facility-modal>
+                <b-icon-envelope-fill></b-icon-envelope-fill>
+                <span class="sr-only">Send an email to {{ data.item.name }}</span>
+              </a>
+            </div>
           </template>
         </b-table>
         <b-pagination v-model="currentPage" :total-rows="rows" :per-page="perPage" class="mt-4"></b-pagination>
@@ -226,17 +239,12 @@ export default {
       this.facilityTypes = [FACILITY_TYPE_ALL];
       this.facilityTypes = this.facilityTypes.concat([...new Set(this.entities.map(entity => entity.type))].sort())
     },
-    sendEmail() {
-      // send emails
-      this.$root.apiPOSTRequest(`/contact/send/entity/${this.actionedFacility.id}`, {}, this.handleSendResponse)
-      // close modal
-      this.$nextTick(() => {
-        this.showEmailErr = false;
-        this.$bvModal.hide('bulk-email-modal')
-      })
-    },
-    sendEmails(bvModalEvt) {
+    sendEmails(bvModalEvt, actionedFacility) {
+      console.log(actionedFacility);
       bvModalEvt.preventDefault()
+      if (this.actionedFacility) {
+        this.selectedEntityIds = [this.actionedFacility.id]
+      }
       if (!this.facilityTypeSelected && this.selectedEntityIds.length === 0) {
         this.showEmailErr = true;
         return
@@ -250,24 +258,19 @@ export default {
         payload.entityType = this.facilityTypeSelected !== FACILITY_TYPE_ALL ? this.facilityTypeSelected : null;
       }
 
-      this.$root.apiPOSTRequest("/contact/send", payload, this.handleBulkSendResponse)
+      this.$root.apiPOSTRequest("/contact/send", payload, this.handleSendEmailsResponse)
       // close modal
       this.$nextTick(() => {
+        // Reset selected Entities
+        this.selectAllEntities = false;
+        this.selectedEntityIds = [];
+        this.actionedFacility = null;
+
         this.showEmailErr = false;
-        this.$bvModal.hide('bulk-email-modal')
+        this.$bvModal.hide('email-facility-modal')
       })
     },
-    handleSendResponse(response) {
-      const title = response.data.results ? 'Success' : 'Error';
-      const variant = response.data.results ? 'success' : 'danger';
-      const msg = response.data.results ? `Success: ${response.data.results.message}` : 'Error: Failed to send email';
-      this.$bvToast.toast(msg, {
-        title,
-        variant,
-        solid: true
-      })
-    },
-    handleBulkSendResponse(response) {
+    handleSendEmailsResponse(response) {
       const title = (response && response.data.results) ? 'Success' : 'Error';
       const variant = (response && response.data.results) ? 'success' : 'danger';
       const msg = (response && response.data.results) ? `Success: Emails sent to ${response.data.results.total} facilities` : 'Error: Failed to send emails';
@@ -360,13 +363,18 @@ p.lead {
   margin: 30px 0 15px;
 }
 
-#bulk-email-modal {
+#email-facility-modal {
   p.error {
     width: 100%;
     margin-top: .25rem;
     font-size: 80%;
     color: #dc3545;
   }
+}
+
+.tableIcons a {
+  margin-right: 5px;
+  color: inherit !important;
 }
 
 </style>
