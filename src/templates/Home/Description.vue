49a37b26543a896ba8a7ca9ddfdf3cca90ef96c6
nmarklund10 <nmarklund10@outlook.com>
2022-03-26 15:49:08 -0400

    created separate components for the home page description and slide show

---
+++ src/templates/Home/Description.vue
@@ -0,0 +1,16 @@
+<template>
+  <div>
+    <h1>Welcome to Healthcare Roll Call</h1>
+    <h4 class="text-muted">Check-in for Healthcare Providers</h4>
+    <p class="lead">
+      <strong>Healthcare Roll Call</strong> allows municipalities to keep track of the status of local healthcare providers connectivity and emergency-related needs. The
+      <strong>Healthcare Roll Call</strong> collects and consolidates all emergency information in one database, allowing outreach coordinators to easily track provider status.
+    </p>
+  </div>
+</template>
+
+<style lang="scss" scoped>
+h1 {
+  font-size: 36px !important;
+}
+</style>
---
+++ src/templates/Home/Tutorial.vue
@@ -0,0 +1,15 @@
+<template>
+  <div>
+    <h5>30 Second Tutorial</h5>
+    <iframe
+      src="https://docs.google.com/presentation/d/e/2PACX-1vQ3Um0pjz8HmUj44qOJ8x0r-P7NUR1kNAYmOCkry8KXcwScoQjmrJ1EMuSVIEsnJOW0GeKXZtN7uAjM/embed?start=true&loop=true&delayms=3000"
+      width="480"
+      height="299"
+      title="tutorial"
+      allowfullscreen="true"
+      mozallowfullscreen="true"
+      webkitallowfullscreen="true"
+      style="max-width:100%;"
+    />
+  </div>
+</template>
