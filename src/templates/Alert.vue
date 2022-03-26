270d5230a33cbf64a1f6b9814bf702a977e82c0b
nmarklund10 <nmarklund10@outlook.com>
2022-03-26 14:33:30 -0400

    move alert into separate component

---
+++ src/templates/Alert.vue
@@ -0,0 +1,43 @@
+<template>
+  <b-alert
+    :show="this.visibleSecondsLeft"
+    dismissible
+    fade
+    :variant="this.type"
+    @dismiss-count-down="this.countDownChanged"
+  >
+    {{this.message}}
+  </b-alert>
+</template>
+
+<script>
+export default {
+  name: "Alert",
+  props: {
+    message: String,
+    type: String,
+  },
+
+  data() {
+    return {
+      visibleAlertLength: 5,
+      visibleSecondsLeft: 0,
+    }
+  },
+
+  methods: {
+    countDownChanged(newCountdown) {
+      this.visibleSecondsLeft = newCountdown
+    },
+    showAlert() {
+      if (this.message.length > 0) {
+        this.visibleSecondsLeft = this.visibleAlertLength
+      }
+    }
+  },
+
+  watch: {
+    message: 'showAlert'
+  }
+}
+</script>
